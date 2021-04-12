9d6b41ccd3202c3f7359993a1fd5a5bb42c84d72
Lisa Bost <bost.lisam@gmail.com>
2021-04-12 13:56:19 -0500

    display selected category

--- Program.cs
+++ Program.cs
@@ -96,6 +96,9 @@ namespace NorthwindConsole
                         int id = int.Parse(Console.ReadLine());
                         Console.Clear();
                         logger.Info($"CategoryId {id} selected");
+
+                        Categories category = db.Categories.FirstOrDefault(c => c.CategoryId == id);
+                        Console.WriteLine($"{category.CategoryName} - {category.Description}");
                     }
                     Console.WriteLine();
 
