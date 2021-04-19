0fc346a558677cd223acd563525dc8db8090d878
Lisa Bost <bost.lisam@gmail.com>
2021-04-19 15:33:20 -0500

    made product name and discontinued required

--- Model/Products.cs
+++ Model/Products.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 // Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
 // If you have enabled NRTs for your project, then un-comment the following line:
@@ -15,6 +16,7 @@ namespace NorthwindConsole.Model
         }
 
         public int ProductId { get; set; }
+        [Required]
         public string ProductName { get; set; }
         public int? SupplierId { get; set; }
         public int? CategoryId { get; set; }
@@ -23,6 +25,7 @@ namespace NorthwindConsole.Model
         public short? UnitsInStock { get; set; }
         public short? UnitsOnOrder { get; set; }
         public short? ReorderLevel { get; set; }
+        [Required]
         public bool Discontinued { get; set; }
 
         public virtual Categories Category { get; set; }
