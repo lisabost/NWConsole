f84f3693e2eede355c0eb64c36d91612f74d9974
Lisa Bost <bost.lisam@gmail.com>
2021-04-19 14:51:56 -0500

    Added prodcuts menu

--- Program.cs
+++ Program.cs
@@ -20,136 +20,160 @@ namespace NorthwindConsole
 
             try
             {
-                string choice;
-                do
+                Console.WriteLine("Choose an option:");
+                Console.WriteLine("1) Categories");
+                Console.WriteLine("2) Products");
+                string option = Console.ReadLine();
+
+                if (option == "1")
                 {
-                    Console.WriteLine("1) Display Categories");
-                    Console.WriteLine("2) Add Category");
-                    Console.WriteLine("3) Display Category and related products");
-                    Console.WriteLine("4) Display all Categories and their related products");
-                    Console.WriteLine("5) Edit a Category");
-                    Console.WriteLine("6) Delete a Category");
-                    Console.WriteLine("\"q\" to quit");
-                    choice = Console.ReadLine();
-                    Console.Clear();
-                    logger.Info($"Option {choice} selected");
-
-                    if (choice == "1")
+                    string choice;
+                    do
                     {
-                        var db = new NWConsole_96_LMBContext();
-                        var query = db.Categories.OrderBy(p => p.CategoryName);
+                        Console.WriteLine("1) Display Categories");
+                        Console.WriteLine("2) Add Category");
+                        Console.WriteLine("3) Display Category and related products");
+                        Console.WriteLine("4) Display all Categories and their related products");
+                        Console.WriteLine("5) Edit a Category");
+                        Console.WriteLine("6) Delete a Category");
+                        Console.WriteLine("\"q\" to quit");
+                        choice = Console.ReadLine();
+                        Console.Clear();
+                        logger.Info($"Option {choice} selected");
 
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.WriteLine($"{query.Count()} records returned");
-                        Console.ForegroundColor = ConsoleColor.Magenta;
-                        foreach (var item in query)
-                        {
-                            Console.WriteLine($"{item.CategoryName} - {item.Description}");
-                        }
-                        Console.ForegroundColor = ConsoleColor.White;
-                    }
-                    else if (choice == "2")
-                    {
-                        logger.Info("User choice: 2 - Enter new Category");
-                        try
+                        if (choice == "1")
                         {
                             var db = new NWConsole_96_LMBContext();
-                            Categories category = InputCategory(db);
+                            var query = db.Categories.OrderBy(p => p.CategoryName);
+
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine($"{query.Count()} records returned");
+                            Console.ForegroundColor = ConsoleColor.Magenta;
+                            foreach (var item in query)
+                            {
+                                Console.WriteLine($"{item.CategoryName} - {item.Description}");
+                            }
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        else if (choice == "2")
+                        {
+                            logger.Info("User choice: 2 - Enter new Category");
+                            try
+                            {
+                                var db = new NWConsole_96_LMBContext();
+                                Categories category = InputCategory(db);
+                                if (category != null)
+                                {
+                                    db.AddCategory(category);
+                                    logger.Info("Category added - {name}", category.CategoryName);
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                logger.Error(ex.Message);
+                            }
+                        }
+                        else if (choice == "3")
+                        {
+                            var db = new NWConsole_96_LMBContext();
+                            var query = db.Categories.OrderBy(p => p.CategoryId);
+
+                            Console.WriteLine("Select the category whose products you want to display:");
+                            Console.ForegroundColor = ConsoleColor.DarkRed;
+                            foreach (var item in query)
+                            {
+                                Console.WriteLine($"{item.CategoryId}) {item.CategoryName}");
+                            }
+                            Console.ForegroundColor = ConsoleColor.White;
+                            int id = int.Parse(Console.ReadLine());
+                            Console.Clear();
+                            logger.Info($"CategoryId {id} selected");
+
+                            Categories category = db.Categories.Include("Products").FirstOrDefault(c => c.CategoryId == id);
+                            Console.WriteLine($"{category.CategoryName} - {category.Description}");
+
+                            if (category.Products.Count() != 0)
+                            {
+                                //Do not display discontinued products when displaying categories and related products
+                                foreach (Products p in category.Products.Where(p => !p.Discontinued))
+                                {
+                                    Console.WriteLine(p.ProductName);
+                                }
+                            }
+                            else
+                            {
+                                //nice display message if there are no products in that category
+                                Console.WriteLine("There are no products in this category.");
+                            }
+                        }
+                        else if (choice == "4")
+                        {
+                            logger.Info("User choice: 4 - Display all categories and related active products.");
+                            var db = new NWConsole_96_LMBContext();
+                            var query = db.Categories.Include("Products").OrderBy(p => p.CategoryId);
+                            foreach (var item in query)
+                            {
+                                Console.WriteLine($"{item.CategoryName}");
+                                //do not display discontinued products
+                                foreach (Products p in item.Products.Where(p => !p.Discontinued))
+                                {
+                                    Console.WriteLine($"\t{p.ProductName}");
+                                }
+                            }
+                        }
+                        else if (choice == "5")
+                        {
+                            logger.Info("User choice: 5 - Edit Category");
+                            //edit category
+                            Console.WriteLine("Choose the category to edit:");
+                            var db = new NWConsole_96_LMBContext();
+                            var category = GetCategory(db);
                             if (category != null)
                             {
-                                db.AddCategory(category);
-                                logger.Info("Category added - {name}", category.CategoryName);
+                                //input new category
+                                Categories UpdatedCategory = InputCategory(db);
+                                if (UpdatedCategory != null)
+                                {
+                                    UpdatedCategory.CategoryId = category.CategoryId;
+                                    db.EditCategory(UpdatedCategory);
+                                    logger.Info($"Category (id: {category.CategoryId}) updated.");
+                                }
                             }
                         }
-                        catch (Exception ex)
+                        else if (choice == "6")
                         {
-                            logger.Error(ex.Message);
-                        }
-                    }
-                    else if (choice == "3")
-                    {
-                        var db = new NWConsole_96_LMBContext();
-                        var query = db.Categories.OrderBy(p => p.CategoryId);
+                            logger.Info("User choice: 6 - Delete Category");
+                            Console.WriteLine("Choose the category to delete");
+                            var db = new NWConsole_96_LMBContext();
+                            var category = GetCategory(db);
+                            if (category != null)
+                            {
+                                //delete category
+                                db.DeleteCategory(category);
 
-                        Console.WriteLine("Select the category whose products you want to display:");
-                        Console.ForegroundColor = ConsoleColor.DarkRed;
-                        foreach (var item in query)
-                        {
-                            Console.WriteLine($"{item.CategoryId}) {item.CategoryName}");
+                                logger.Info($"Category (id: {category.CategoryId}) deleted.");
+                            }
                         }
-                        Console.ForegroundColor = ConsoleColor.White;
-                        int id = int.Parse(Console.ReadLine());
+                        Console.WriteLine();
+
+                    } while (choice.ToLower() != "q");
+                }
+                else if (option == "2")
+                {
+                    //Products time
+                    string choice;
+                    do
+                    {
+                        Console.WriteLine("1) Display Products");
+                        Console.WriteLine("2) Add Product");
+                        Console.WriteLine("3) Edit Product");
+                        Console.WriteLine("\"q\" to quit");
+                        choice = Console.ReadLine();
                         Console.Clear();
-                        logger.Info($"CategoryId {id} selected");
-
-                        Categories category = db.Categories.Include("Products").FirstOrDefault(c => c.CategoryId == id);
-                        Console.WriteLine($"{category.CategoryName} - {category.Description}");
-
-                        if (category.Products.Count() != 0)
-                        {
-                            //Do not display discontinued products when displaying categories and related products
-                            foreach (Products p in category.Products.Where(p => !p.Discontinued))
-                            {
-                                Console.WriteLine(p.ProductName);
-                            }
-                        }
-                        else
-                        {
-                            //nice display message if there are no products in that category
-                            Console.WriteLine("There are no products in this category.");
-                        }
+                        logger.Info($"Option {choice} selected");
                     }
-                    else if (choice == "4")
-                    {
-                        logger.Info("User choice: 4 - Display all categories and related active products.");
-                        var db = new NWConsole_96_LMBContext();
-                        var query = db.Categories.Include("Products").OrderBy(p => p.CategoryId);
-                        foreach (var item in query)
-                        {
-                            Console.WriteLine($"{item.CategoryName}");
-                            //do not display discontinued products
-                            foreach (Products p in item.Products.Where(p => !p.Discontinued))
-                            {
-                                Console.WriteLine($"\t{p.ProductName}");
-                            }
-                        }
-                    }
-                    else if (choice == "5")
-                    {
-                        logger.Info("User choice: 5 - Edit Category");
-                        //edit category
-                        Console.WriteLine("Choose the category to edit:");
-                        var db = new NWConsole_96_LMBContext();
-                        var category = GetCategory(db);
-                        if (category != null)
-                        {
-                            //input new category
-                            Categories UpdatedCategory = InputCategory(db);
-                            if (UpdatedCategory != null)
-                            {
-                                UpdatedCategory.CategoryId = category.CategoryId;
-                                db.EditCategory(UpdatedCategory);
-                                logger.Info($"Category (id: {category.CategoryId}) updated.");
-                            }
-                        }
-                    }
-                    else if (choice == "6")
-                    {
-                        logger.Info("User choice: 6 - Delete Category");
-                        Console.WriteLine("Choose the category to delete");
-                        var db = new NWConsole_96_LMBContext();
-                        var category = GetCategory(db);
-                        if (category != null)
-                        {
-                            //delete category
-                            db.DeleteCategory(category);
-
-                            logger.Info($"Category (id: {category.CategoryId}) deleted.");
-                        }
-                    }
-                    Console.WriteLine();
-
-                } while (choice.ToLower() != "q");
+                    while (choice.ToLower() != "q");
+                }
             }
             catch (Exception ex)
             {
